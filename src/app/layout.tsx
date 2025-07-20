16b370e1f3279b1e2b0eff7d9b048878ba878591
Bruno Postali <bruno.postali@sbxgroup.com>
2025-07-20 00:14:24 -0300

    added adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7164529681401190"
+                    crossOrigin="anonymous"
+                ></script>
+                ;
+            </head>
             <body className={`${roboto.className} antialiased bg-primary`}>
                 {children}
             </body>
