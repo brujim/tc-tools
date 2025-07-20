d8b62a804f47e8b33d238ad61827ae6577137b76
Bruno Postali <bruno.postali@sbxgroup.com>
2025-07-20 00:17:22 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <script
-                    async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7164529681401190"
-                    crossOrigin="anonymous"
-                ></script>
-                ;
+                <meta
+                    name="google-adsense-account"
+                    content="ca-pub-7164529681401190"
+                />
             </head>
             <body className={`${roboto.className} antialiased bg-primary`}>
                 {children}
