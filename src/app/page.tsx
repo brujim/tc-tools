58659e890fa62f7599d0721106f15faf71b0bf3f
Bruno Postali <bruno.postali@sbxgroup.com>
2025-07-20 16:49:41 -0300

    changed the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+"use client";
+
 import { BidCalc } from "@/components/BidCalc";
+import { Hero } from "@/components/Hero";
+import { University } from "@/components/University";
+import { useState } from "react";
 
 export default function Home() {
+    const [component, setComponent] = useState("");
+
     return (
         <div>
-            <main>
-                <BidCalc />
+            <main className="pt-4 pb-10">
+                <Hero />
+                <nav className="flex items-center justify-center gap-2 flex-wrap px-2 mb-4">
+                    <button
+                        className="border-2 py-2 px-8 rounded-sm text-sub border-sub bg-secondary font-sans font-bold cursor-pointer transform hover:scale-105 duration-200"
+                        onClick={() => setComponent("bid")}
+                    >
+                        BID CALCULATOR
+                    </button>
+                    <button
+                        className="border-2 py-2 px-8 rounded-sm text-sub border-sub bg-secondary font-sans font-bold cursor-pointer transform hover:scale-105 duration-200"
+                        onClick={() => setComponent("uni")}
+                    >
+                        UNIVERSITYS
+                    </button>
+                </nav>
+                {component === "bid" && <BidCalc />}
+                {component === "uni" && <University />}
             </main>
             <footer></footer>
         </div>
